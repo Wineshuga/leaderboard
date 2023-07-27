a11cbdbe4d60e91a8e15618a5224cdcbbe07e372
wineshuga <uzochukwuwinnie@gmail.com>
2023-07-27 02:33:32 +0100

    Add event listeners and modules to index.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
 import './style.css';
 import leaderLists from './modules/leadersLists.js';
+import { getScores, postScores } from './modules/dataFunctions.js';
+import createGame from './modules/createGame.js';
 
-leaderLists();
+let gameId = '';
+const gameInfo = { name: 'wineshuga crush' };
+createGame(gameInfo).then((id) => {
+  gameId = id.result.slice(14, -7);
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const username = document.querySelector('#your-name');
+  const userScore = document.querySelector('#your-score');
+
+  const leader = {
+    user: username.value,
+    score: userScore.value,
+  };
+  postScores(leader, gameId);
+  username.value = '';
+  userScore.value = '';
+});
+
+const refreshBtn = document.querySelector('.lists > button');
+refreshBtn.addEventListener('click', () => {
+  getScores(gameId).then((data) => {
+    const leaderData = data.result;
+    leaderLists(leaderData);
+  });
+});
