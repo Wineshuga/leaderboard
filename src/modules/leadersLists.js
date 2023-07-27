022f27beb15711501b0ee8b19409cdd121ca01fb
wineshuga <uzochukwuwinnie@gmail.com>
2023-07-27 02:32:44 +0100

    Add leadersLists.js file

--- src/modules/leadersLists.js
+++ src/modules/leadersLists.js
@@ -1,44 +1,14 @@
-const leaderData = [
-  {
-    name: 'Name: ',
-    score: 100,
-  },
-  {
-    name: 'Name: ',
-    score: 60,
-  },
-  {
-    name: 'Name: ',
-    score: 58,
-  },
-  {
-    name: 'Name: ',
-    score: 120,
-  },
-  {
-    name: 'Name: ',
-    score: 10,
-  },
-  {
-    name: 'Name: ',
-    score: 99,
-  },
-  {
-    name: 'Name: ',
-    score: 30,
-  },
-];
-
-const leaderLists = () => {
+const leaderLists = (leaderData) => {
   const leaderUl = document.createElement('ul');
   leaderUl.className = 'leaders-list';
   leaderData.forEach((leader) => {
     leaderUl.innerHTML += `
-      <li>${leader.name} ${leader.score}</li>
+      <li> ${leader.user}: ${leader.score}</li>
     `;
   });
-  const ul = document.querySelector('.lists');
-  ul.appendChild(leaderUl);
+  const leadersBox = document.querySelector('.leaders-box');
+  leadersBox.innerHTML = '';
+  leadersBox.appendChild(leaderUl);
 };
 
 export default leaderLists;
